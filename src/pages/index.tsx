12cb4e627b4670bb6f4b001c065c97a45247b9ad
Danillo Ramos <danilloramosbezerra@gmail.com>
2021-04-22 12:35:03 -0300

    Consume API with Next SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,30 @@
-export default function Home() {
+export default function Home(props) {
+  // SPA - Não aparece na indexação do Google
+  // useEffect(() => {
+  //   fetch('http://localhost:3333/episodes')
+  //     .then(response => response.json())
+  //     .then(data => console.log(data))
+  // }, []);
+
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+// SSG - Static Site Generation
+// Para que não faça uma nova requisição toda vez que alguém acessar a página,
+// assim como é feito no SSR - Server Side Rendering
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8 // atualiza a cada 8 horas
+  }
 }
